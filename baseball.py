35da59e83787e85f18794d9853c3a435fb68ed65
jason-sa <jandlsalazeradams@gmail.com>
2018-10-02 14:28:45 -0700

    added imports

--- baseball.py
+++ baseball.py
@@ -1,3 +1,12 @@
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+import time
+import os
+from selenium.webdriver.common.by import By
 import pickle
 
 def get_boxscore_urls(driver):
