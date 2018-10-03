71e5a2c767c552f1cf6bdbfaf36b15279f17d1e9
jason-sa <jandlsalazeradams@gmail.com>
2018-10-03 08:55:53 -0700

    added build_rookie_table

--- baseball.py
+++ baseball.py
@@ -19,3 +19,22 @@ def get_boxscore_urls(driver):
 def write_pkl(data, name):
     with open('pickles/'+name+'.pkl', 'wb') as picklefile:
         pickle.dump(data, picklefile)
+
+def build_rookie_table(start, end, driver):
+#     urls = []
+    rookie_df = pd.DataFrame(columns=['Name','Debut','Age','Tm','rookie_year'])
+    for i in range(start, end+1):
+#         urls.append('https://www.baseball-reference.com/leagues/MLB/'+str(i)+'-rookies.shtml')
+        url = 'https://www.baseball-reference.com/leagues/MLB/'+str(i)+'-rookies.shtml'
+        print('Scraping', url)
+        driver.get(url)
+
+        batting = driver.find_element_by_id('misc_batting')
+        batting_df = pd.read_html(batting.get_attribute('outerHTML'))
+        
+        year_df = batting_df[0].loc[:,['Name','Debut','Age','Tm']]
+        year_df['rookie_year'] = [i] * batting_df[0].shape[0]
+#         print(year_df.head())
+        rookie_df = rookie_df.append(year_df)
+#         rookie_df.head()        
+    return rookie_df 
