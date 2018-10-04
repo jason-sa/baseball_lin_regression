aac472ae288cb0691afe1020ee6a714c00470ffa
jason-sa <jandlsalazeradams@gmail.com>
2018-10-03 17:03:51 -0700

    added function to create HTML data frames

--- baseball.py
+++ baseball.py
@@ -38,3 +38,62 @@ def build_rookie_table(start, end, driver):
         rookie_df = rookie_df.append(year_df)
 #         rookie_df.head()        
     return rookie_df 
+
+def build_rookie_pages(start, end, driver):
+    rookie_pages = pd.DataFrame(columns=['year','link','html'])
+    rookie_player_pages = pd.DataFrame(columns=['year','name','link','html'])
+    
+    #attempt to load from csv
+    try:
+        rookie_pages = pd.read_csv('data/rookie_pages.csv')
+    except FileNotFoundError:
+        pass
+    print(rookie_pages.shape)
+    try:
+        rookie_player_pages = pd.read_csv('data/rookie_player_pages.csv')
+    except FileNotFoundError:
+        pass
+    print(rookie_player_pages.shape)
+    
+    for i in range(start, end+1):
+        links_list = []
+        names_list = []
+        
+        #if year == i, then move onto link loop
+        if not (rookie_pages.year == i).any():
+            print('new rookie page data')
+            url = 'https://www.baseball-reference.com/leagues/MLB/'+str(i)+'-rookies.shtml'
+#             print('Scraping', url)
+            driver.get(url)
+            rookie_pages.loc[i] = [i, url, driver.page_source]
+
+            # scrape the rookie batters (includes pitchers if PA)
+            batting = driver.find_element_by_id('misc_batting') ## HTML tables
+            links = batting.find_elements_by_xpath('.//tbody/tr/td/a') ## player pages
+
+            # add these to the DF to save
+            links_list = [a.get_attribute('href') for a in links if re.search(r'players/.', a.get_attribute('href'))]
+            names_list = [a.text for a in links if re.search(r'players/.', a.get_attribute('href'))]
+
+            rookie_pages.to_csv('data/rookie_pages.csv')
+        
+        if len(links_list) != 0: # add new data
+            index = rookie_player_pages.index.max()+1
+            index_l = list(range(index, index+len(links_list)+1))
+            year_l = [i] * len(links_list)
+            rookie_player_pages.loc[index_l] = [year_l, names_list, links_list, np.nan()]
+
+        # loop only over incomplete data
+#         index = rookier_player_pages.index.max() + 1
+        
+        for l in rookie_player_pages.loc[(rookie_player_pages.html == np.nan), 'link'].values;
+            driver.get(l)
+#             print(names_list[k])
+            rookie_player_pages.loc[index, 'html'] = driver.page_source
+            if index != 0 and index % 10 == 0:
+                rookie_player_pages.to_csv('data/rookie_player_pages.csv')
+            index += 1
+        
+        rookie_player_pages.to_csv('data/rookie_player_pages.csv')
+
+    return rookie_pages, rookie_player_pages
