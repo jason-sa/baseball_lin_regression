79489991a05753b71d709bac449f64b602e2544c
jason-sa <jandlsalazeradams@gmail.com>
2018-10-14 21:52:11 -0700

    add documentation

--- baseball.py
+++ baseball.py
@@ -7,7 +7,6 @@ import requests
 import time
 import os
 from selenium.webdriver.common.by import By
-import pickle
 import re
 import time
 from sklearn.linear_model import LinearRegression
@@ -90,7 +89,10 @@ def get_player_data(html, year, name):
 
 def build_rookie_table(rookie_pages):
     ''' Builds a master data set of all rookie players using the rookie summary page on baseballreference.com
-    rookie_pages: pd.DataFrame containing 
+    
+    rookie_pages: pd.DataFrame containing [html, year]
+
+    return: pd.DataFrame() ['Name','Debut','Age','Tm','rookie_year']
     '''
     rookie_df = pd.DataFrame(columns=['Name','Debut','Age','Tm','rookie_year'])
     rookie_dfs = []
@@ -121,6 +123,15 @@ def build_rookie_table(rookie_pages):
     return rookie_df
 
 def get_player_salary(html, year, name, ind):
+    ''' Parses a player's page on baseballrefernce.com and builds a data frame of their salary data
+
+    html: player page from baseballreference.com
+    year: rookie year
+    name: player name
+    ind: index to build a unique identfier
+
+    return: appends to /data/salary.csv
+    '''
     salary_soup = BeautifulSoup(html, 'lxml')
 
     salary_html = salary_soup.find('table',{'id':'br-salaries'})
@@ -157,16 +168,18 @@ def get_player_salary(html, year, name, ind):
         with open(PATH_S, 'a') as f:
             salary_df.to_csv(f, header=False)
 
-def partion_rookie_players(size, df, name):
-    tot = df.shape[0]
+def run_models(X_train, y_train, name, results = None, cv=10, alphas=[10**a for a in range(-2,5)]):
+    ''' Method to quickly run all models with different feature sets. 
+    Runs: OLS, Standard Scaler + LassoCV, and PolynomialFeatures + Standard Scaler + LassoCV
 
-    i = 0
-    while i <= tot:
-        df.iloc[i:i+size].to_pickle('data/pickles/'+ name + '_' + str(i//size) + '.pkl')
-    i += size
+    X_train: training feature set
+    y_train: training actuals
+    name: name of the type of run (used for comparing different feature sets)
+    results: data frame to hold the results if varying the feature set
+    cv: number of n-fold cross validations
+    alphas: range of alpha values for CV
 
-def run_models(X_train, y_train, name, results = None, cv=10, alphas=np.logspace(-3, -0.5, 10)):
-    ''' Method to quickly run all models with different feature sets X_train
+    return: pd.DataFrame of the MSE results
     '''    
     # capture the results for the feature set
     model_results = pd.Series(name=name)
@@ -176,29 +189,20 @@ def run_models(X_train, y_train, name, results = None, cv=10, alphas=np.logspace
     lin_model = LinearRegression()
     scores = cross_val_score(lin_model, X_train, y_train, cv=cv, scoring='neg_mean_squared_error')
     model_results['linear model - cv10'] = np.mean(-scores)
-    # model_results
 
-    # Build a Lasso model with standard scaling.
 
-    # Now perform a 10-fold cross validation for same range of alphas.
-
-    # probably want to change this range based on GridCV
-    # alphas = [10**a for a in range(-2,5)]
-
+    # Now perform a n-fold cross validation
     cv_lasso = make_pipeline(StandardScaler(), LassoCV(cv=cv, alphas=alphas, tol=0.001))
     cv_lasso.fit(X_train, y_train)
     model_results['lasso cv - ' + str(cv_lasso.get_params()['lassocv'].alpha_)] = mean_mse_Lasso(cv_lasso, 'lassocv')
-    # model_results.sort_values()
 
-    # Now 2-5 degree polynomial features and perform a 10-fold cross validation.
+    # Now 2-5 degree polynomial features and perform a n-fold cross validation.
 
     for degrees in range(2,6):
         cv_lasso_poly = make_pipeline(PolynomialFeatures(degrees), StandardScaler(), LassoCV(cv=cv, alphas=alphas,tol=0.001))
         cv_lasso_poly.fit(X_train, y_train)
         model_results['lasso poly ' + str(degrees) + ' cv - ' + str(cv_lasso_poly.get_params()['lassocv'].alpha_)] = mean_mse_Lasso(cv_lasso_poly, 'lassocv')
 
-    # model_results.sort_values()
-    
     if results is None:
         results = pd.DataFrame(model_results)
     else:
@@ -207,7 +211,14 @@ def run_models(X_train, y_train, name, results = None, cv=10, alphas=np.logspace
     return results
 
 def mean_mse_Lasso(model,name):
+    ''' Calcualtes the MSE of an n-fold CV from LassoCV model
+    model: sklearn model or pipeline
+    name: name of the lassocv model
+
+    return float64 (mean MSE)
+    '''
     mse = model.get_params()[name].mse_path_
     alphas = model.get_params()[name].alphas_
     mse_df = pd.DataFrame(data=mse, index=alphas)
+
     return mse_df.loc[model.get_params()[name].alpha_].mean()
